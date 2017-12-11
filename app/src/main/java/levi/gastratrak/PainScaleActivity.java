8b2f69fd02a636e6b0689a51da1fed5e1ff209a9
Buffalo2391 <buffalo2391@gmail.com>
2017-12-11 21:55:22 +1300

    Applied new save button to pain and stool

--- app/src/main/java/levi/gastratrak/PainScaleActivity.java
+++ app/src/main/java/levi/gastratrak/PainScaleActivity.java
@@ -3,6 +3,8 @@ package levi.gastratrak;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -15,13 +17,24 @@ public class PainScaleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pain_scale);
-        Button saveButton = findViewById(R.id.painSaveButton);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.return_to_main_saved, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_accept:
                 saveButtonPressed();
-            }
-        });
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     private void saveButtonPressed() {
--- app/src/main/java/levi/gastratrak/StoolRecordActivity.java
+++ app/src/main/java/levi/gastratrak/StoolRecordActivity.java
@@ -3,6 +3,8 @@ package levi.gastratrak;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -13,15 +15,26 @@ public class StoolRecordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stool_record);
-        Button saveButton = findViewById(R.id.saveStoolButton);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                saveButtonPressed();
-            }
-        });
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.return_to_main_saved, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_accept:
+                saveButtonPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
     private void saveButtonPressed() {
         SeekBar consistencySeeker = findViewById(R.id.consistencySeeker);
         SeekBar wetnessSeeker = findViewById(R.id.wetnessSeeker);
